f64b605b80446db8c1917a607674978a56911420
M. Afifudin <afif.x.udin@gmail.com>
2024-08-17 15:46:32 +0700

    ci: deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://afiiif.github.io',
+  base: 'dom-dom-factory',
   integrations: [
     tailwind({
       nesting: true,
